897d637da018f3926a8bd49ab99453ae7c6099e7
seda yegin <seda__yegin@hotmail.com>
2022-07-04 14:03:59 +0300

    feat: Add random assignment

--- Seda.MVC.UI/Controllers/HomeController.cs
+++ Seda.MVC.UI/Controllers/HomeController.cs
@@ -7,20 +7,38 @@ namespace Seda.MVC.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Random _rnd;
 
         private readonly IAssignmentService _assignmentService;
         public HomeController(IAssignmentService assignmentService)
         {
+            _rnd = new Random();
             _assignmentService = assignmentService;
         }
 
         public IActionResult Index()
         {
-
             var assignments = _assignmentService.GetAssignments();
+
+            if (assignments.Count == 0)
+                for (int i = 1; i < 9; i++)
+                {
+                    _assignmentService.AddAssignemntAsync(new AssignmentViewModel { Difficulty = _rnd.Next(1, 9), PersonalId = _rnd.Next(1, 500), Name = "Task" + i });
+                }
+
+            assignments = _assignmentService.GetAssignments();
 
             return View(assignments);
         }
+
+        public IActionResult AddRandomAssignment()
+        {
+            _assignmentService.AddAssignemntAsync(new AssignmentViewModel { Difficulty = _rnd.Next(1, 9), PersonalId = _rnd.Next(1, 500), Name = "Task" + _rnd.Next(1, 90) });
+            var assignments = _assignmentService.GetAssignments();
+            return View(assignments);
+        }
+
+
 
         public IActionResult Privacy()
         {
