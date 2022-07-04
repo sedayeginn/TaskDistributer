6aa9650260fa90f8ac9dabc4106ba39c040b3f67
seda yegin <seda__yegin@hotmail.com>
2022-07-04 15:38:56 +0300

    ekle çıkar ekranı düzenlendi

---
+++ Seda.MVC.UI/Controllers/AssignmentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seda.MVC.UI.Interfaces;
+using Seda.MVC.UI.Models;
+
+namespace Seda.MVC.UI.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly IAssignmentService _assignmentService;
+
+        public AssignmentController(IAssignmentService assignmentService)
+        {
+            _assignmentService = assignmentService;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var assignments = _assignmentService.GetAssignments();
+
+            return View(assignments);
+        }
+
+        [HttpGet]
+        public IActionResult NewAssignment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewAssignment(AssignmentViewModel assignment)
+        {
+            _assignmentService.AddAssignemntAsync(assignment);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteAssignment(int id)
+        {
+            _assignmentService.RemoveAssignemntAsync(id);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
